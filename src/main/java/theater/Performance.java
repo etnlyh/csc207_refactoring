51d62478080eca57965811761070b0006e3d839e
ethan <ethxnoo9@gmail.com>
2025-11-22 14:04:04 -0500

    Task 1.1 done

--- src/main/java/theater/Performance.java
+++ src/main/java/theater/Performance.java
@@ -5,11 +5,23 @@ package theater;
  */
 public class Performance {
 
-    public String playID;
-    public int audience;
+    private String playID;
+    private int audience;
 
     public Performance(String playID, int audience) {
         this.playID = playID;
+        this.setAudience(audience);
+    }
+
+    public String getPlayID() {
+        return playID;
+    }
+
+    public int getAudience() {
+        return audience;
+    }
+
+    public void setAudience(int audience) {
         this.audience = audience;
     }
 }
